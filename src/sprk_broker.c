f52108d88ae1cdf37ffe876ec6818af9e9f6aecc
Matt Forbes <matt@tellapart.com>
2016-01-31 16:45:20 -0800

    .

--- src/sprk_broker.c
+++ src/sprk_broker.c
@@ -5,11 +5,10 @@ main (int argc, char *argv[])
 {
     setvbuf(stdout, NULL, _IONBF, 0);
 
-    assert (argc == 3);
     char contexts_uri[256];
     char executors_uri[256];
-    snprintf (contexts_uri, 256, "tcp://127.0.0.1:%s", argv[1]);
-    snprintf (executors_uri, 256, "tcp://127.0.0.1:%s", argv[2]);
+    snprintf (contexts_uri, 256, "tcp://0.0.0.0:%s", argv[1]);
+    snprintf (executors_uri, 256, "tcp://0.0.0.0:%s", argv[2]);
 
     broker_t *broker = broker_new (contexts_uri, executors_uri);
     broker_run (broker);
